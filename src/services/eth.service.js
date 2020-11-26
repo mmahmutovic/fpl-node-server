23ec31dc9bf79312f3141230c5ac06e058dce624
Mustafa Mahmutovic <mustafa.mahmutovic93@gmail.com>
2020-11-26 13:53:45 +0100

    Added built contracts to node server

--- src/services/eth.service.js
+++ src/services/eth.service.js
@@ -1,35 +1,49 @@
 const Web3 = require('web3');
-// const Betting = require('../../node_modules/fpl-contract/build/contracts/Betting.json');
+const Agenda = require('agenda');
+const Betting = require('../../contracts/Betting.json');
 
 const etherUrl = 'http://localhost:8545';
-// const contractAddress = Betting.networks[5777].address;
+const contractAddress = Betting.networks[5777].address;
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(etherUrl));
-// const contractInstance = new web3.eth.Contract(Betting.abi, contractAddress);
+const contractInstance = new web3.eth.Contract(Betting.abi, contractAddress);
 const db = require('../db/db');
 
-// const profit = 1.02;
+const profit = 1.02;
 const { Bet } = db;
 
 async function createGame(req, res) {
-  // const resultHash = await contractInstance.methods.balanceOf(contractAddress)
-  // .send({ from: '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1' });
+  const resultHash = await contractInstance.methods.balanceOf(contractAddress)
+    .send({ from: '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1' });
   const bet = new Bet();
-  bet.betId = 1;
-  // bet.betId = resultHash.betId;
+  bet.betId = resultHash.betId;
   bet.betAmount = req.body.betAmount;
   bet.unlockDate = new Date(req.body.unlockDate).getTime();
   bet.owner = req.user.sub;
   await bet.save();
-  // await contractInstance.createGame(req.body.amount, bet.unlockDate)
-  // .send({ from: req.body.address, to: contractInstance,
-  // value: web3.utils.toWei(req.body.amount * profit, 'ether') });
-  // await contractInstance.transferToContract(req.body.amount)
-  //   .send({
-  //     from: req.body.address,
-  //     to: contractAddress,
-  //     value: web3.utils.toWei(req.body.amount * profit, 'ether'),
-  //   });
+  await contractInstance.createGame(req.body.amount, bet.unlockDate)
+    .send({
+      from: req.body.address,
+      to: contractInstance,
+      value: web3.utils.toWei(req.body.amount * profit, 'ether'),
+    });
+  await contractInstance.transferToContract(req.body.amount)
+    .send({
+      from: req.body.address,
+      to: contractAddress,
+      value: web3.utils.toWei(req.body.amount * profit, 'ether'),
+    });
+
+  // Add Agenda that will call payout when the deadline is there
+  const agenda = new Agenda().mongo(db, 'jobs');
+
+  // Wait for agenda to connect. Should never fail since connection failures
+  // should happen in the `await MongoClient.connect()` call.
+  await new Promise((resolve) => agenda.once('ready', resolve));
+  agenda.schedule(new Date(Date.now() + 1000), 'print', {
+    message: 'Hello!',
+  });
+
   return res;
 }
 
@@ -43,13 +57,13 @@ async function acceptInvite(req, res) {
   const bet = await Bet.findOneAndUpdate({ _id: req.body.betId }, {
     $push: { players: req.user.sub },
   });
-  // await contractInstance.acceptInvite().send({ from: req.body.address });
-  // await contractInstance.transferToContract(req.body.amount * profit)
-  //   .send({
-  //     from: req.body.address,
-  //     to: contractAddress,
-  //     value: web3.utils.toWei(req.body.amount * profit, 'ether'),
-  //   });
+  await contractInstance.acceptInvite().send({ from: req.body.address });
+  await contractInstance.transferToContract(req.body.amount * profit)
+    .send({
+      from: req.body.address,
+      to: contractAddress,
+      value: web3.utils.toWei(req.body.amount * profit, 'ether'),
+    });
   await bet.save();
   return res.status(200).send(bet);
 }
